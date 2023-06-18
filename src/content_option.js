46dabb868e11d17660fa6a1623eb8386c78cdb58
Hrishita Panjetha <hrishita.panjetha@gmail.com>
2023-06-18 18:52:25 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,113 @@
+const logotext = "Hrishita Panjetha";
+const meta = {
+    title: "Hrishita Panjetha",
+    description: "I’m Hrishita Panjetha web developer _ Full stack devloper,currently in final year of engineering",
+};
+
+const introdata = {
+    title: "I’m Hrishita Panjetha",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web applications",
+    },
+    description: "Highly motivated and passionate computer science enthusiast with a strong foundation in programming and problem-solving. Seeking an opportunity to leverage my technical skills, creativity, and continuous learning mindset to contribute to a dynamic organization and make a meaningful impact in the field of computer science.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "Highly motivated and passionate computer science enthusiast with a strong foundation in programming and problem-solving. Seeking an opportunity to leverage my technical skills, creativity, and continuous learning mindset to contribute to a dynamic organization and make a meaningful impact in the field of computer science.",
+};
+const worktimeline = [{
+        jobtitle: "Finance and Sponsorship Coordinator",
+        where: "Riviera",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+    },
+    {
+        title: "Web Apps",
+    },
+    {
+        title: "React apps",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Word count app",
+        link: "https://react-tasks-lake.vercel.app/task1",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Calculator app",
+        link: "https://react-tasks-lake.vercel.app/task2",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Table to get values",
+        link: "https://react-tasks-lake.vercel.app/task4",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Age calculator app",
+        link: "https://react-tasks-lake.vercel.app/task5",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hrishita.panjetha@gmail.com",
+    YOUR_FONE: "+91 8307636340",
+
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/hrishitapanjetha",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/hrishitapanjetha/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
